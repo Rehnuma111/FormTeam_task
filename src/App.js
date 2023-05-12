056dbf5afd81e574d5ee859fce3401d317d2ab82
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-05-12 22:49:52 +0530

    Task is submitted by rehnuma

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from './Component/Header';
+import Layout from './Component/Layout';
+import { useState } from 'react';
+import data from "./heliverse_mock_data.json";
+
+const App = () => {
+  const [filter, setFilter] = useState('');
+
+    const OnChangeInput = (event) => {
+        setFilter(event.target.value);
+    };
+
+    const filteredData = data.filter((item) => {
+        const itemName = item.first_name.toLowerCase() + item.last_name.toLocaleLowerCase();
+        const itemGender = item.gender.toLowerCase();
+        const itemAvailability = JSON.stringify(item.available).toLocaleLowerCase();
+        const itemDomain = item.domain.toLocaleLowerCase()
+        const filterText = filter.toLowerCase();
+
+        return (
+            itemName.includes(filterText) ||
+            itemGender.includes(filterText) || itemDomain.includes(filterText) || itemAvailability.includes(filterText)
+        );
+    });
+
+  return (
+    <React.Fragment>
+      <Header fun={OnChangeInput}/>
+      <Layout data={filteredData}/>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/Component/Layout.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import '../style/Layout.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { incPage} from '../Actions';
+
+const Layout = (props) => {
+  const myState = useSelector((state)=>state.ChangeThePage);
+  const dispatch = useDispatch()
+
+  const [currentPage, setCurrentPage] = useState(myState);
+  const PER_PAGE = 20;
+  const offset = currentPage * PER_PAGE;
+  const pageCount = Math.ceil(props.data.length / PER_PAGE);
+
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
+    dispatch(incPage(selectedPage));
+  };
+
+  return (
+    <>
+      <div className="grid-container">
+        {props.data.slice(offset, offset + PER_PAGE).map((item) => (
+          <div className="card" key={item.id}>
+            <img src={item.avatar} alt="Avatar" />
+            <div className="card-info">
+              <h2>{item.first_name} {item.last_name}</h2>
+              <p className="email">Email: {item.email}</p>
+              <p>Gender: {item.gender}</p>
+              <p>Domain: {item.domain}</p>
+              <p>Available: <span className="unavailable">{JSON.stringify(item.available)}</span></p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <ReactPaginate
+        previousLabel="Previous"
+        nextLabel="Next"
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName="pagination"
+        previousLinkClassName="previous-page"
+        nextLinkClassName="next-page"
+        disabledClassName="pagination-disabled"
+        activeClassName="pagination-active"
+      />
+    </>
+  );
+};
+
+export default Layout;
